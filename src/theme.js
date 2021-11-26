aa8202b1f8a2e1b0c4f5c945d80efaba7c8a31b5
Lester Fernandez <5958256+lesterfernandez@users.noreply.github.com>
2021-11-26 15:55:05 -0500

    setup

---
+++ src/theme.js
@@ -0,0 +1,24 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const theme = {
+  config: {
+    initialColorMode: "dark",
+    useSystemColorMode: true,
+  },
+  styles: {
+    global: {
+      code: {
+        fontFamily:
+          "source-code-pro, Menlo, Monaco, Consolas, 'Courier New',monospace",
+      },
+      body: {
+        fontFamily:
+          "-apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',sans-serif",
+        WebkitFontSmoothing: "antialiased",
+        MozOsxFontSmoothing: "grayscale",
+      },
+    },
+  },
+};
+
+export default extendTheme(theme);
